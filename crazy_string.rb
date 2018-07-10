bed617f17c7947215e5a0d7ca63af0e5edd9a062
mggbhn <maggiebswim@gmail.com>
2018-07-10 16:02:49 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -4,4 +4,4 @@ def crazy_strings(a,b)
   "#{a}" "#{b}"
 end
 
-puts crazy_strings("hello".upcase.reverse, "friends".capitalize.swapcase.gsub(S,Z))
+puts crazy_strings("hello".upcase.reverse, "friends".gsub(s,z).capitalize.swapcase)
